713e918e8e9814a185a0fea1511813f3c85db0d9
Thiago Martins de Melo <thiago.melo@terceiro.rnp.br>
2023-02-14 17:06:52 -0300

    feat: configuração pre-commit

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.54.0"
     }
   }
+
+  backend "s3" {
+    bucket = "comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
