fb174f37785b9a27a3244e1a1608fcff208336f8
Thiago Martins de Melo <thiago.melo@terceiro.rnp.br>
2023-02-15 10:45:00 -0300

    feat: region e tags

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+
+    tags = {
+        Department = "Devops"    
+        Organization = "Infrastructure and Operations"
+        Project = "EKS"
+        Environment = "Development"
+    }
+}
--- provider.tf
+++ provider.tf
@@ -11,7 +11,6 @@ terraform {
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
-
 }
 
 provider "aws" {
